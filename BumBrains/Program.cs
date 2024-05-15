c89b7bf6828562536b289b791cecda06d4ff3026
isiguta <isihuta@gmail.com>
2024-05-14 18:58:51 -0600

    Initial functions commit

---
+++ BumBrains/Program.cs
@@ -0,0 +1,19 @@
+using BumBrains;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddHttpClient<Authenticate>(client =>
+        {
+
+        });
+    })
+    .Build();
+
+host.Run();
